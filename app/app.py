a11a497dee0a9a629a08cffe91d162684b1b979b
Naritomo Miura <naritomo@NaritomonoMacBook-Air.local>
2020-08-25 18:48:05 +0900

    Day4終了

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+from models.models import OnegaiContent
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -7,15 +8,19 @@ app = Flask(__name__)
 
 #「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
-def hello():
-    return "Hello World"
-
 
 #「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html",name=name,all_onegai=all_onegai)
 
+@app.route("/index",methods=["post"])
+def post():
+    name = request.form["name"]
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
 
 #app.pyをターミナルから直接呼び出した時だけ、app.run()を実行する
 if __name__ == "__main__":
